449016a8ae313c788adac7b12aab1efcb723a8de
Paulo Sergio <pscosta2008@gmail.com>
2025-05-18 13:10:48 -0300

    feat: adding api basic file

---
+++ MaskApiTest/src/main/java/com/masktestapi/MaskApiTest/MaskApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.masktestapi.MaskApiTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaskApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaskApiTestApplication.class, args);
+	}
+
+}
---
+++ MaskApiTest/src/test/java/com/masktestapi/MaskApiTest/MaskApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masktestapi.MaskApiTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaskApiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
